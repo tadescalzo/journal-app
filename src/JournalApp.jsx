bc4b406aef2fe4631b9b3b8903f351e6457ea6a6
tadescalzo <ta.descalzo@gmail.com>
2023-03-15 23:28:15 -0300

    configuracion basica de mui

---
+++ src/JournalApp.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+import {AppRouter} from './router/AppRouter'
+import { AppTheme } from './theme';
+
+
+export const JournalApp = () => {
+  return (
+    <BrowserRouter>
+      <AppTheme>
+        <AppRouter/>
+      </AppTheme>
+    </BrowserRouter>
+  )
+}
---
+++ src/theme/AppTheme.jsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { purpleTheme } from './';
+
+export const AppTheme = ({ children }) => {
+  return (
+    <ThemeProvider theme={purpleTheme}>
+    <CssBaseline />
+    {children}
+    </ThemeProvider>
+  )
+}
