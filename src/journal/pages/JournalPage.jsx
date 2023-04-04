aa3e6ff0daa3050a10f03350ccdb11d37be6f92b
tadescalzo <ta.descalzo@gmail.com>
2023-04-04 20:54:12 -0300

    a

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -10,23 +10,9 @@ export const JournalPage = () => {
     <>
     <JournalLayout>
 
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
       <NoteView />
-
-      {/* <IconButton 
-        size="large"
-        sx={{
-          color:'white',
-          backgroundColor:'error.main',
-          ':hover' : {backgroundColor:'error.main',opacity: '0.7'},
-          position:'fixed',
-          right:50,
-          bottom:50
-        }}
-      >
-        <AddOutlined sx={{fontSize:30}} />
-      </IconButton> */}
 
       <Fab 
         size="large"
